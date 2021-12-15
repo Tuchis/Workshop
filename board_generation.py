e5945c9c183bbb9c4b1a7e1e05bf567269c6168a
mousey <91616572+anastasiasenyk@users.noreply.github.com>
2021-12-15 10:30:40 +0200

    board_g

--- board_generation.py
+++ board_generation.py
@@ -1,18 +1,26 @@
 import string
 
-lst = []
-timed_lst = []
-let_list = ['  ']
-for i in range(1, 11):
-    let_list.append(string.ascii_uppercase[i - 1])
-    i = str(i)
-    if len(i) < 2:
-        i = '0' + i
-    timed_lst.append(i)
-    for j in range(1, 11):
-        timed_lst.append('_')
-    lst.append(timed_lst)
+def board_generate():
+    lst = []
     timed_lst = []
-lst.insert(0, let_list)
-for line in lst:
-    print(*line)
+    let_list = ['  ']
+    for i in range(1, 11):
+        let_list.append(string.ascii_uppercase[i - 1])
+        i = str(i)
+        # if len(i) < 2:
+        #     i = '0' + i
+        # timed_lst.append(i)
+        for j in range(1, 11):
+            timed_lst.append('_')
+        lst.append(timed_lst)
+        timed_lst = []
+    lst.insert(0, let_list)
+    for index, line in enumerate(lst):
+        line_str = ''
+        for item in line:
+            line_str +=item + ' '
+        if index<10:
+            print('0' + str(index)+ ' ' + line_str)
+        else:
+            print(str(index) + ' ' + line_str)
+board_generate()
