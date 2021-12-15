bad7a7cbf1ce6ceb77f3a3c587134bdabd5a15de
Dimitro2021 <91617001+Dimitro2021@users.noreply.github.com>
2021-12-15 11:48:31 +0200

    Update board_generation.py

--- board_generation.py
+++ board_generation.py
@@ -1,5 +1,5 @@
 import string
-
+import sys
 
 def generate_field():
     lst = []
@@ -11,12 +11,37 @@ def generate_field():
         if len(i) < 2:
             i = '0' + i
         timed_lst.append(i)
-        for j in range(1, 11):
+        for _ in range(1, 11):
             timed_lst.append('_')
         lst.append(timed_lst)
         timed_lst = []
     lst.insert(0, let_list)
     return lst
+
+def win_checker(field):
+    """ return true if field isn't dead yet """
+    flag = False
+    for lst in field:
+        for ele in lst:
+            if ele == '*':
+                flag = True
+    return flag
+
+def cord_asker():
+    print('You have to make a move. If you want to leave the game, enter exit')
+    column = 0
+    row = 'a'
+    alph = 'ABCDEFGIJ'
+    row_exe = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10']
+    while alph.find(column) == -1 and column != 'exit':
+        column = input('Input the letter')
+    while row not in row_exe and row != 'exit':
+        row = input('Input the row')
+    if row == 'exit' or column == 'exit':
+        print('byee')
+        sys.exit()
+    return column, row
+
 
 def print_field(field):
     for line in field:
@@ -27,7 +52,3 @@ def place_figures(player, field):
     moves = 0
     while moves < 4:
         pass
-
-
-
-
